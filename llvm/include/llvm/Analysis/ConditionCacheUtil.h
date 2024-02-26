189999fe05751f3d6949a37208dc27ed1f1df9ee
Noah Goldstein <goldstein.w.n@gmail.com>
2024-02-26 16:37:46 -0600

    Cache: And/Or

--- llvm/include/llvm/Analysis/ConditionCacheUtil.h
+++ llvm/include/llvm/Analysis/ConditionCacheUtil.h
@@ -49,7 +49,8 @@ static void findValuesAffectedByCondition(
     if (match(V, m_Not(m_Value(X))))
       AddAffected(X);
 
-    if (!IsAssume && match(V, m_LogicalOp(m_Value(A), m_Value(B)))) {
+    // No point adding if we aren't going to be able to analyze
+    if (match(V, m_LogicalOp(m_Value(A), m_Value(B)))) {
       Worklist.push_back(A);
       Worklist.push_back(B);
     } else if (match(V, m_Cmp(Pred, m_Value(A), m_Value(B)))) {
