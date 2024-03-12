297c1880b036470aad63f776a3ebfe850fe8bf6c
Noah Goldstein <goldstein.w.n@gmail.com>
2024-03-12 13:27:46 -0500

    Is it Cond?

--- llvm/lib/Analysis/ValueTracking.cpp
+++ llvm/lib/Analysis/ValueTracking.cpp
@@ -1028,8 +1028,8 @@ static void computeKnownBitsFromOperator(const Operator *I,
       computeKnownBits(Arm, Res, Depth + 1, Q);
       if (isGuaranteedNotToBeUndef(Arm, Q.AC, Q.CxtI, Q.DT, Depth + 1)) {
         // See what condition implies about the bits of the two select arms.
-        computeKnownBitsFromCond(Arm, I->getOperand(0), Res, Depth + 1, Q,
-                                 Invert);
+        //computeKnownBitsFromCond(Arm, I->getOperand(0), Res, Depth + 1, Q,
+        //                         Invert);
         // We can have conflict if the condition is dead. I.e if we have
         // (x | 64) < 32 ? (x | 64) : y
         // we will have conflict at bit 6 from the condition/the `or`.
